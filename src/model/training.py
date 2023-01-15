7189b0ca7f9454b90fa7dc5e2a3aa6d2cbb87dc1
Maya Yan <104634888+mayahkg@users.noreply.github.com>
2023-01-15 20:25:12 +0800

    Base Model Main Struture code
    - Code has Errors
    - @realhuman101 edit 4 main branches
    - @Maya finish input layer

--- src/model/training.py
+++ src/model/training.py
@@ -17,10 +17,52 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 import os
-
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
 from .datasets import *
 
+# Loading in the training data
+x_train = np.asarray(training_data['temp', 'RH', 'wind', 'rain'])
+y_train = np.asarray(training_data['area'])
 
-# Loading in the data
-x = np.asarray(training_data['temp', 'RH', 'wind', 'rain'])
-y = np.asarray(training_data['area'])
+# define 4  sets of inputs
+inputA = x_train['temp'].shape[1]
+inputB = x_train['temp'].shape[1]
+inputC = x_train['temp'].shape[1]
+inputD = x_train['temp'].shape[1]
+
+# the first branch operates on the first input
+x = Dense(8, activation="relu")(inputA)
+x = Dense(4, activation="relu")(x)
+x = Model(inputs=inputA, outputs=x)
+
+# the second branch opreates on the second input
+y = Dense(64, activation="relu")(inputB)
+y = Dense(32, activation="relu")(y)
+y = Dense(4, activation="relu")(y)
+y = Model(inputs=inputB, outputs=y)
+
+# the 3rd branch opreates on the second input
+y = Dense(64, activation="relu")(inputB)
+y = Dense(32, activation="relu")(y)
+y = Dense(4, activation="relu")(y)
+y = Model(inputs=inputC, outputs=y)
+
+# the 4th branch opreates on the second input
+y = Dense(64, activation="relu")(inputB)
+y = Dense(32, activation="relu")(y)
+y = Dense(4, activation="relu")(y)
+y = Model(inputs=inputD, outputs=y)
+
+# combine the output of the 4 branches
+combined = concatenate([x.output, y.output, z.output, t.output])
+
+# apply a FC layer and then a regression prediction on the
+# combined outputs
+z = Dense(2, activation="relu")(combined)
+z = Dense(1, activation="linear")(z)
+
+# our model will accept the inputs of the 4 branches and
+# then output a single value
+model = Model(inputs=[x.input, y.input], outputs=z)
