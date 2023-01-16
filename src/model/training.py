99c1ef06a79837e81c379960fca60fa1cef25f7b
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2023-01-17 07:56:15 +0800

    Fixed import errors

--- src/model/training.py
+++ src/model/training.py
@@ -18,8 +18,8 @@ import numpy as np
 import pandas as pd
 import os
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, concatenate
+from keras import Model
 from .datasets import *
 
 # Loading in the training data
