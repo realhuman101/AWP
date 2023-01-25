62d6a3ebed934b56a1a9629bf2590baa420d3dac
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2023-01-25 21:21:34 +0800

    Fixed import errors

--- src/model/training.py
+++ src/model/training.py
@@ -20,7 +20,7 @@ import os
 from keras.models import Sequential
 from keras.layers import Input, Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 from keras import Model
-from datasets import *
+from .datasets import *
 
 # Loading in the training data
 temp_train = np.asarray(training_data['temp'])
