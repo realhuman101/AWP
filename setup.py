e79d252063b497d7284ef6673d6dc2253637cd17
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2023-01-26 15:14:16 +0800

    Fixed requirements
    Removed depreciated requirements

--- setup.py
+++ setup.py
@@ -55,7 +55,10 @@ setuptools.setup(
 		"numpy>=1.24.1",
 		"pandas>=1.5.2",
 		"requests>=2.28.1",
-		"sklearn>=0.0"
+		"matplotlib>=3.5.1",
+		"scikit-learn>=1.2.1",
+		"keras>=2.11.0",
+		"scipy>=1.10.0"
     ],
 	entry_points = {
 		'gui_scripts': [
