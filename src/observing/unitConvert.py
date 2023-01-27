cbd8e0dd5d3365d4c235cd7c60acc78124c4ddd7
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2023-01-27 16:04:51 +0800

    Created conversion function for wind

---
+++ src/observing/unitConvert.py
@@ -0,0 +1,38 @@
+def convertWind(windSpeed: int) -> int:
+	match windSpeed:
+		case 0:
+			windSpeed = 0
+		case 1:
+			windSpeed = 3
+		case 2:
+			windSpeed = 9
+		case 3:
+			windSpeed = 16
+		case 4:
+			windSpeed = 24
+		case 5:
+			windSpeed = 34
+		case 6:
+			windSpeed = 44
+		case 7:
+			windSpeed = 56
+		case 8:
+			windSpeed = 68
+		case 9:
+			windSpeed = 82
+		case 10:
+			windSpeed = 96
+		case 11:
+			windSpeed = 110
+		case 12:
+			windSpeed = 124
+		case 13:
+			windSpeed = 141
+		case 14:
+			windSpeed = 157
+		case 15:
+			windSpeed = 175
+		case 16:
+			windSpeed = 192
+	
+	return windSpeed
