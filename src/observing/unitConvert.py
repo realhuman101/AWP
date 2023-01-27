e0b5355ebae020e819d64e73516d85e211252897
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2023-01-27 21:25:37 +0800

    Fix observing violations

--- src/observing/unitConvert.py
+++ src/observing/unitConvert.py
@@ -2,7 +2,7 @@
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, version 3 of the License.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -54,5 +54,5 @@ def convertWind(windSpeed: int) -> int:
 			windSpeed = 175
 		case 16:
 			windSpeed = 192
-	
+
 	return windSpeed
