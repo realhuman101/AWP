f82f99ee63a13c962b13195ef7df11b852cb8314
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2023-01-31 18:44:52 +0800

    Convert indentation to tabs

--- src/gui/__init__.py
+++ src/gui/__init__.py
@@ -17,31 +17,31 @@ from .pages.manualInput import manual_input
 from .pages.predictions import prediction
 
 def start() -> None:
-    # Opening the main window
-    window = tk.Tk()
-    window.title('AWP')
-    window.attributes('-fullscreen',True)
-    width = window.winfo_screenwidth()
-    height = window.winfo_screenheight()
+	# Opening the main window
+	window = tk.Tk()
+	window.title('AWP')
+	window.attributes('-fullscreen',True)
+	width = window.winfo_screenwidth()
+	height = window.winfo_screenheight()
 
-    # Setting the background color
-    canvas = tk.Canvas(window, width = width, height = height, bg ='#545454')
-    canvas.place(relx = 0.5, rely = 0.5, anchor = 'center')
+	# Setting the background color
+	canvas = tk.Canvas(window, width = width, height = height, bg ='#545454')
+	canvas.place(relx = 0.5, rely = 0.5, anchor = 'center')
 
-    # Creating the main text
-    label = tk.Label(window, text='AUTOMATED FOREST', bg='#545454', fg='white', font=('Times new roman', 30))
-    label.place(relx = 0.25,rely = 0.47, anchor = 'center')
-    label = tk.Label(window, text='FIRE PREDICTION', bg='#545454', fg='white',  font=('Times new roman', 30))
-    label.place(relx = 0.25,rely = 0.53, anchor = 'center')
+	# Creating the main text
+	label = tk.Label(window, text='AUTOMATED FOREST', bg='#545454', fg='white', font=('Times new roman', 30))
+	label.place(relx = 0.25,rely = 0.47, anchor = 'center')
+	label = tk.Label(window, text='FIRE PREDICTION', bg='#545454', fg='white',  font=('Times new roman', 30))
+	label.place(relx = 0.25,rely = 0.53, anchor = 'center')
 
-    # Creating the Rectangle
-    canvas = tk.Canvas(window, width = width/2, height = height, bg ='#323466')
-    canvas.place(relx = 0.75, rely = 0.5, anchor = 'center')
+	# Creating the Rectangle
+	canvas = tk.Canvas(window, width = width/2, height = height, bg ='#323466')
+	canvas.place(relx = 0.75, rely = 0.5, anchor = 'center')
 
-    # Creating buttons
-    button1 = tk.Button(window, text='MANUAL PREDICTION', font=('Times new roman', 20), borderwidth=0, command=lambda:manual_input(window, width, height))
-    button1.place(relx = 0.76, rely = 0.32, anchor = 'center')
-    button2 = tk.Button(window, text='AUTOMATED PREDICTION', font=('Times new roman', 20), borderwidth=0, command=lambda:prediction(window, width, height))
-    button2.place(relx = 0.76, rely = 0.63, anchor = 'center')
+	# Creating buttons
+	button1 = tk.Button(window, text='MANUAL PREDICTION', font=('Times new roman', 20), borderwidth=0, command=lambda:manual_input(window, width, height))
+	button1.place(relx = 0.76, rely = 0.32, anchor = 'center')
+	button2 = tk.Button(window, text='AUTOMATED PREDICTION', font=('Times new roman', 20), borderwidth=0, command=lambda:prediction(window, width, height))
+	button2.place(relx = 0.76, rely = 0.63, anchor = 'center')
 
-    window.mainloop()
+	window.mainloop()
