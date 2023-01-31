14e393d077893b262887b1adfad03ef285e7721f
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2023-01-31 18:10:34 +0800

    Convert spaces to tabs

--- src/gui/pages/predictions/future.py
+++ src/gui/pages/predictions/future.py
@@ -17,40 +17,40 @@ from tkinter import ttk
 
 
 def future(window: tk.Tk, width: int, height: int) -> None:
-    # Setting the background color
-    canvas = tk.Canvas(window, width = width, height = height, bg ='#545454')
-    canvas.place(relx = 0.5, rely = 0.5, anchor = 'center')
+	# Setting the background color
+	canvas = tk.Canvas(window, width = width, height = height, bg ='#545454')
+	canvas.place(relx = 0.5, rely = 0.5, anchor = 'center')
 
-    # Line 
-    canvas = tk.Canvas(window, width = width/5*4, height = 0.01, bg ='#323466')
-    canvas.place(relx = 0.5, rely = 0.15, anchor = 'center')
+	# Line 
+	canvas = tk.Canvas(window, width = width/5*4, height = 0.01, bg ='#323466')
+	canvas.place(relx = 0.5, rely = 0.15, anchor = 'center')
 
-    # Creating the title
-    label = tk.Label(window, text='FUTURE DATA PREDICTION', bg='#545454', fg='white', font=('Times new roman', 30))
-    label.place(relx = 0.5,rely = 0.1, anchor = 'center')
+	# Creating the title
+	label = tk.Label(window, text='FUTURE DATA PREDICTION', bg='#545454', fg='white', font=('Times new roman', 30))
+	label.place(relx = 0.5,rely = 0.1, anchor = 'center')
 
-    # Creating radio buttons
-    v = tk.StringVar(window, "1")
+	# Creating radio buttons
+	v = tk.StringVar(window, "1")
  
-    # Dictionary to create multiple buttons
-    values = {"1" : "1", "2" : "2", "3" : "3", "4" : "4", "5" : "5", "6" : "6", "7" : "7", "8" : "8", "9" : "9",}
-    
-    # Creating style element
-    s = ttk.Style()   
-    s.configure('style.TRadiobutton',   
-        background='#545454',     
-        foreground='white', font=('Times new roman', 20)) 
+	# Dictionary to create multiple buttons
+	values = {"1" : "1", "2" : "2", "3" : "3", "4" : "4", "5" : "5", "6" : "6", "7" : "7", "8" : "8", "9" : "9",}
+	
+	# Creating style element
+	s = ttk.Style()   
+	s.configure('style.TRadiobutton',   
+		background='#545454',     
+		foreground='white', font=('Times new roman', 20)) 
 
-    # rather than creating each button separately
-    x = 1
-    for (text, value) in values.items():
-        tk.Radiobutton(window, text = text, variable = v, style='style.TRadiobutton',
-            value = value).place(relx = x/10, rely = 0.5, anchor = 'center')
-        x += 1
+	# rather than creating each button separately
+	x = 1
+	for (text, value) in values.items():
+		tk.Radiobutton(window, text = text, variable = v, style='style.TRadiobutton',
+			value = value).place(relx = x/10, rely = 0.5, anchor = 'center')
+		x += 1
 
-    # Creating button
-    button1 = tk.Button(window, text='    PREDICT    ', font=('Times new roman', 20), borderwidth=0, command = window.destroy)
-    button1.place(relx = 0.5, rely = 0.75, anchor = 'center')
+	# Creating button
+	button1 = tk.Button(window, text='    PREDICT    ', font=('Times new roman', 20), borderwidth=0, command = window.destroy)
+	button1.place(relx = 0.5, rely = 0.75, anchor = 'center')
 
-    window.mainloop()
-    
+	window.mainloop()
+	
--- src/gui/pages/predictions/present.py
+++ src/gui/pages/predictions/present.py
@@ -17,41 +17,41 @@ from tkinter import *
 
 
 def present(window: tk.Tk, width: int, height: int) -> None:
-    # Setting the background color
-    canvas = tk.Canvas(window, width = width, height = height, bg ='#545454')
-    canvas.place(relx = 0.5, rely = 0.5, anchor = 'center')
+	# Setting the background color
+	canvas = tk.Canvas(window, width = width, height = height, bg ='#545454')
+	canvas.place(relx = 0.5, rely = 0.5, anchor = 'center')
 
-    # Line 
-    canvas = tk.Canvas(window, width = width/5*4, height = 0.01, bg ='#323466')
-    canvas.place(relx = 0.5, rely = 0.15, anchor = 'center')
+	# Line 
+	canvas = tk.Canvas(window, width = width/5*4, height = 0.01, bg ='#323466')
+	canvas.place(relx = 0.5, rely = 0.15, anchor = 'center')
 
-    # Creating the title
-    label = tk.Label(window, text='PRESENT DATA PREDICTION', bg='#545454', fg='white', font=('Times new roman', 30))
-    label.place(relx = 0.5,rely = 0.1, anchor = 'center')
+	# Creating the title
+	label = tk.Label(window, text='PRESENT DATA PREDICTION', bg='#545454', fg='white', font=('Times new roman', 30))
+	label.place(relx = 0.5,rely = 0.1, anchor = 'center')
   
-    # Dropdown menu options
-    options = [
-        "Chose a reigon:",
-        "New territory",
-        "Tung Chung",
-        "Sai Kung",
-        "Kowloon",
-        "Sai wan",
-        "Wong Tai Sin",
-    ]
-    
-    # datatype of menu text
-    clicked = StringVar()
-    clicked.set( "Chose a reigon:" )
-    
-    # Create Dropdown menu
-    drop = OptionMenu(window , clicked , *options)
-    drop.config(font=('Times new roman', 20), borderwidth=0, width=20)
-    drop.place(relx = 0.5, rely = 0.4, anchor = 'center')
+	# Dropdown menu options
+	options = [
+		"Chose a reigon:",
+		"New territory",
+		"Tung Chung",
+		"Sai Kung",
+		"Kowloon",
+		"Sai wan",
+		"Wong Tai Sin",
+	]
+	
+	# datatype of menu text
+	clicked = StringVar()
+	clicked.set( "Chose a reigon:" )
+	
+	# Create Dropdown menu
+	drop = OptionMenu(window , clicked , *options)
+	drop.config(font=('Times new roman', 20), borderwidth=0, width=20)
+	drop.place(relx = 0.5, rely = 0.4, anchor = 'center')
 
-     # Creating buttons
-    button1 = tk.Button(window, text='    PREDICT    ', font=('Times new roman', 20), borderwidth=0, command = window.destroy)
-    button1.place(relx = 0.5, rely = 0.75, anchor = 'center')
+	 # Creating buttons
+	button1 = tk.Button(window, text='    PREDICT    ', font=('Times new roman', 20), borderwidth=0, command = window.destroy)
+	button1.place(relx = 0.5, rely = 0.75, anchor = 'center')
 
-    window.mainloop()
-    
+	window.mainloop()
+	
