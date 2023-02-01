2c28a62e08bb626faff17ba26e06cbed61f06567
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2023-02-01 10:34:10 +0800

    Create base backbutton

---
+++ src/gui/pages/assets/backbutton.py
@@ -0,0 +1,23 @@
+# ======================================================================
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ======================================================================
+
+import tkinter as tk
+
+
+def backbutton(window: tk.Tk, x: int, y: int, prev_page: function, anchor: str = 'center') -> None:
+	# Create buttons
+	button = tk.Button(window, text='← Back', font=('Montserrat', 10), borderwidth=0, command=prev_page)
+	button.place(relx=x, rely=y, anchor=anchor)
+
+	window.mainloop()
