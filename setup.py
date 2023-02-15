d150dd79c38b1ec7f9c28df8f33ccae3bd33e883
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2023-02-15 13:54:08 +0800

    Updated requirements to include updated tf and keras

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setuptools.setup(
 	license_files=('LICENSE', 'COPYING'),
 	license='GPL v3',
 	install_requires=[
-		"tensorflow>=2.10.1",
+		"tensorflow>=2.11.0",
 		"numpy>=1.24.1",
 		"pandas>=1.5.2",
 		"requests>=2.28.1",
