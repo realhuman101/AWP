91658f6c3d469cb8c07c23e3b5bf38ceb7c7f2d0
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2023-02-15 18:50:14 +0800

    Fixed rounded button formatting issues

--- src/gui/pages/assets/roundedbutton.py
+++ src/gui/pages/assets/roundedbutton.py
@@ -14,87 +14,86 @@
 
 import tkinter as tk
 
+
 class RoundedButton(tk.Canvas):
-    def __init__(self, master=None, text:str="", font:tuple[str,int]=("Montserrat",30), radius=25, btnforeground="#000000", btnbackground="#ffffff", bg='#545454', clicked=None, *args, **kwargs):
-        super(RoundedButton, self).__init__(master, *args, **kwargs)
-        self.config(bg=bg)
-        self.btnbackground = btnbackground
-        self.clicked = clicked
+	def __init__(self, master=None, text: str = "", font: tuple[str, int] = ("Montserrat", 30), radius=25, btnforeground="#000000", btnbackground="#ffffff", bg='#545454', clicked=None, *args, **kwargs):
+		super(RoundedButton, self).__init__(master, *args, **kwargs)
+		self.config(bg=bg)
+		self.btnbackground = btnbackground
+		self.clicked = clicked
 
-        self.radius = radius        
-        
-        self.rect = self.round_rectangle(0, 0, 0, 0, tags="button", radius=radius, fill=btnbackground)
-        self.text = self.create_text(0, 0, text=text, tags="button", fill=btnforeground, font=font, justify="center")
+		self.radius = radius
 
-        self.tag_bind("button", "<ButtonPress>", self.border)
-        self.tag_bind("button", "<ButtonRelease>", self.border)
-        self.bind("<Configure>", self.resize)
-        
-        text_rect = self.bbox(self.text)
-        if int(self["width"]) < text_rect[2]-text_rect[0]:
-            self["width"] = (text_rect[2]-text_rect[0]) + 10
-        
-        if int(self["height"]) < text_rect[3]-text_rect[1]:
-            self["height"] = (text_rect[3]-text_rect[1]) + 10
-          
-    def round_rectangle(self, x1, y1, x2, y2, radius=25, update=False, **kwargs):
-        points = [x1+radius, y1,
-                x1+radius, y1,
-                x2-radius, y1,
-                x2-radius, y1,
-                x2, y1,
-                x2, y1+radius,
-                x2, y1+radius,
-                x2, y2-radius,
-                x2, y2-radius,
-                x2, y2,
-                x2-radius, y2,
-                x2-radius, y2,
-                x1+radius, y2,
-                x1+radius, y2,
-                x1, y2,
-                x1, y2-radius,
-                x1, y2-radius,
-                x1, y1+radius,
-                x1, y1+radius,
-                x1, y1]
+		self.rect = self.round_rectangle(0, 0, 0, 0, tags="button", radius=radius, fill=btnbackground)
+		self.text = self.create_text(0, 0, text=text, tags="button", fill=btnforeground, font=font, justify="center")
 
-        if not update:
-            return self.create_polygon(points, **kwargs, smooth=True)
-        
-        else:
-            self.coords(self.rect, points)
+		self.tag_bind("button", "<ButtonPress>", self.border)
+		self.tag_bind("button", "<ButtonRelease>", self.border)
+		self.bind("<Configure>", self.resize)
 
-    def resize(self, event):
-        text_bbox = self.bbox(self.text)
+		text_rect = self.bbox(self.text)
+		if int(self["width"]) < text_rect[2] - text_rect[0]:
+			self["width"] = (text_rect[2] - text_rect[0]) + 10
 
-        if self.radius > event.width or self.radius > event.height:
-            radius = min((event.width, event.height))
+		if int(self["height"]) < text_rect[3] - text_rect[1]:
+			self["height"] = (text_rect[3] - text_rect[1]) + 10
 
-        else:
-            radius = self.radius
+	def round_rectangle(self, x1, y1, x2, y2, radius=25, update=False, **kwargs):
+		points = [x1 + radius, y1,
+				x1 + radius, y1,
+				x2 - radius, y1,
+				x2 - radius, y1,
+				x2, y1,
+				x2, y1 + radius,
+				x2, y1 + radius,
+				x2, y2 - radius,
+				x2, y2 - radius,
+				x2, y2,
+				x2 - radius, y2,
+				x2 - radius, y2,
+				x1 + radius, y2,
+				x1 + radius, y2,
+				x1, y2,
+				x1, y2 - radius,
+				x1, y2 - radius,
+				x1, y1 + radius,
+				x1, y1 + radius,
+				x1, y1]
 
-        width, height = event.width, event.height
+		if not update:
+			return self.create_polygon(points, **kwargs, smooth=True)
+		else:
+			self.coords(self.rect, points)
 
-        if event.width < text_bbox[2]-text_bbox[0]:
-            width = text_bbox[2]-text_bbox[0] + 30
-        
-        if event.height < text_bbox[3]-text_bbox[1]:  
-            height = text_bbox[3]-text_bbox[1] + 30
-        
-        self.round_rectangle(5, 5, width-5, height-5, radius, update=True)
+	def resize(self, event):
+		text_bbox = self.bbox(self.text)
 
-        bbox = self.bbox(self.rect)
+		if self.radius > event.width or self.radius > event.height:
+			radius = min((event.width, event.height))
+		else:
+			radius = self.radius
 
-        x = ((bbox[2]-bbox[0])/2) - ((text_bbox[2]-text_bbox[0])/2)
-        y = ((bbox[3]-bbox[1])/2) - ((text_bbox[3]-text_bbox[1])/2)
+		width, height = event.width, event.height
 
-        self.moveto(self.text, x, y)
+		if event.width < text_bbox[2] - text_bbox[0]:
+			width = text_bbox[2] - text_bbox[0] + 30
 
-    def border(self, event):
-        if event.type == "4":
-            self.itemconfig(self.rect, fill="#d2d6d3")
-            if self.clicked is not None:
-                self.clicked()
-        else:
-            self.itemconfig(self.rect, fill=self.btnbackground)
+		if event.height < text_bbox[3] - text_bbox[1]:
+			height = text_bbox[3] - text_bbox[1] + 30
+
+		self.round_rectangle(5, 5, width - 5, height - 5, radius, update=True)
+
+		bbox = self.bbox(self.rect)
+
+		x = ((bbox[2] - bbox[0]) / 2) - ((text_bbox[2] - text_bbox[0]) / 2)
+		y = ((bbox[3] - bbox[1]) / 2) - ((text_bbox[3] - text_bbox[1]) / 2)
+
+		self.moveto(self.text, x, y)
+
+	def border(self, event):
+		if event.type == "4":
+			self.itemconfig(self.rect, fill="#d2d6d3")
+			if self.clicked is not None:
+				self.clicked()
+		else:
+			self.itemconfig(self.rect, fill=self.btnbackground)
