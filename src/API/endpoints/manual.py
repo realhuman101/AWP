168c2ea6d349c3611523e9ffc5a0e4d019aca64b
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2023-02-19 21:19:38 +0800

    Fixed manual input

--- src/API/endpoints/manual.py
+++ src/API/endpoints/manual.py
@@ -12,6 +12,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # ======================================================================
 
+from flask import request
 from flask_restful import Resource, reqparse
 import numpy as np
 import os
@@ -19,15 +20,9 @@ from keras.models import load_model
 
 class Manual(Resource):
 	def get(self):
-		# Add arguments
-		parser = reqparse.RequestParser()
-
-		parser.add_argument('temp', type=int, help='Temperature (Celsius) -> int')
-		parser.add_argument('wind', type=int, help='Wind Speed (km/h) -> int')
-		parser.add_argument('rain', type=float, help='Rain Size (mm) -> float')
-		parser.add_argument('rh', type=int, help='Relative Humidity (%) -> int')
-
-		args = parser.parse_args()
+		# Get arguments
+		args = dict(request.args)
+		args = {key:float(val) for key,val in args.items()} 
 
 		# Check if all arguments are present
 		if all([args['temp'], args['wind'], args['rain'], args['rh']]) is False:
@@ -39,4 +34,4 @@ class Manual(Resource):
 
 		prediction = model.predict(data)
 
-		return {'data': prediction}, 200
+		return {'data': str(prediction[0][0])}, 200
